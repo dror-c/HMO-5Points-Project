4f177dfb1e655797f7b1cc4da4617ce8207b304d
dror <User@DESKTOP-1UJ2P8K>
2020-11-15 18:09:23 +0200

    add generic therapy database

---
+++ DrorCohen/DB/GenericTherapyDB.cs
@@ -0,0 +1,59 @@
+﻿using DrorCohen.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrorCohen.DB
+{
+    class GenericTherapyDB: GeneralDB
+    {
+        public GenericTherapyDB() : base("GenericTherapy", "TherapyCode") { }
+        public new GenericTherapy GetCurrentRow()
+        {
+            return new GenericTherapy(base.GetCurrentRow());
+        }
+        //החזרת מפתח ראשי האחרון
+        public /*int*/ string GetKey()
+        {
+            int x = currentRow;
+            GoToLast();
+            //int key = Convert.ToInt32(base.GetCurrentRow()[primaryKey]) + 1;
+            string key = Convert.ToString(Convert.ToInt32(base.GetCurrentRow()[primaryKey]) + 1);
+            currentRow = x;
+            return key;
+        }
+        public string GetKeyName()
+        {
+            return "TherapyCode";
+        }
+        public void Update(GenericTherapy cos)
+        {
+            DataRow dr = base.GetCurrentRow();
+            cos.Populate(dr);
+        }
+        //public void Add(Customer cus)
+        //{
+        //    DataRow dr = base.Add(cus);
+        //    cus.Populate(dr);
+        //    //base.Add(dr);
+        //    Find(cus.Id);
+        //}
+        //public static DataTable GetAllCustomer()
+        //{
+        //    DataTable tb;
+        //    string sqlStat = "select customer_ID, last_name + ' ' + first_name as [fullName] From customers ";
+        //    tb = DAL.GetInstance().GetDisplayTable("customer", sqlStat);
+        //    return tb;
+        //}
+        //public DataView GetDataView()
+        //{
+        //    DAL d = DAL.GetInstance();
+        //    DataTable a = d.GetDisplayTable("customers", "Select customer_ID, first_name,last_name From customers");
+        //    return new DataView(table);
+
+        //}
+    }
+}
--- DrorCohen/Models/GenericTherapy.cs
+++ DrorCohen/Models/GenericTherapy.cs
@@ -1,18 +1,29 @@
-﻿using System;
+﻿using DrorCohen.DB;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DrorCohen.Models
 {
-    class GenericTherapy
+    public class GenericTherapy:IEntity
     {
         private string therapyCode;
         private string nameOftherapy;
         private string durationOfTherapy;
         private string typeOfTherapy;
         private string whoCanGiveTheTherapy;
+        public GenericTherapy(DataRow dr)
+        {
+            this.TherapyCode = dr["TherapyCode"].ToString();
+            this.nameOftherapy = dr["NameOftherapy"].ToString();
+            this.durationOfTherapy = dr["DurationOfTherapy"].ToString();
+            this.typeOfTherapy = dr["TypeOfTherapy"].ToString();
+            this.whoCanGiveTheTherapy = dr["WhoCanGiveTheTherapy"].ToString();
+        }
+
         public string TherapyCode
         {
             set { this.therapyCode = value; }
@@ -37,6 +48,14 @@ namespace DrorCohen.Models
         {
             set { this.whoCanGiveTheTherapy = value; }
             get { return this.whoCanGiveTheTherapy; }
+        }
+        public virtual void Populate(DataRow dr)
+        {
+            dr["TherapyCode"] = TherapyCode;
+            dr["NameOftherapy"] = NameOftherapy;
+            dr["DurationOfTherapy"] = DurationOfTherapy;
+            dr["TypeOfTherapy"] = TypeOfTherapy;
+            dr["WhoCanGiveTheTherapy"] = WhoCanGiveTheTherapy;
         }
     }
 }
